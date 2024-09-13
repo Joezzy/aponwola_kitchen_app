bd596c9fb23561a7c979df58f42a38c0571d8355
joezzy <onalojoseph96@gmail.com>
2024-09-13 02:09:56 +0100

    bloc rm

--- lib/view/product/productList/bloc/product_list_state.dart
+++ lib/view/product/productList/bloc/product_list_state.dart
@@ -1,34 +1,34 @@
-part of 'product_list_bloc.dart';
-
-abstract class ProductListState extends Equatable {
-  const ProductListState();
-}
-
-
-
-
-class ProductListInitialState extends ProductListState {
-  @override
-  List<Object> get props => [];
-}
-class ProductListLoadingState extends ProductListState {
-  @override
-  List<Object> get props => [];
-}
-
-class ProductListLoadedState extends ProductListState {
-  final List<Product> model;
-  const ProductListLoadedState(this.model);
-
-  @override
-  // TODO: implement props
-  List<Object?> get props => [];
-}
-
-// class ProductListErrorState extends ProductListState {
-//   late final String msg;
-//   ProductListErrorState (this.msg)
+// part of 'product_list_bloc.dart';
+//
+// abstract class ProductListState extends Equatable {
+//   const ProductListState();
+// }
+//
+//
+//
+//
+// class ProductListInitialState extends ProductListState {
+//   @override
+//   List<Object> get props => [];
+// }
+// class ProductListLoadingState extends ProductListState {
+//   @override
+//   List<Object> get props => [];
+// }
+//
+// class ProductListLoadedState extends ProductListState {
+//   final List<Product> model;
+//   const ProductListLoadedState(this.model);
 //
 //   @override
-//   List<Object> get props => [msg];
+//   // TODO: implement props
+//   List<Object?> get props => [];
 // }
+//
+// // class ProductListErrorState extends ProductListState {
+// //   late final String msg;
+// //   ProductListErrorState (this.msg)
+// //
+// //   @override
+// //   List<Object> get props => [msg];
+// // }
